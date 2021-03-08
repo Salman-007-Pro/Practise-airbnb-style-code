072c31083a10306500796c4ba224f8692ff9a481
Salman Asif <salmanasif36@gmail.com>
2021-03-08 17:27:56 +0500

    error code not push with eslint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('Salman');
+  const obj = 'asd';
+  // console.log('Salman');
   return (
     <div className="App">
       <header className="App-header">
