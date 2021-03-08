2459bcdb10075a0c07cfbcfd37ffefea563e5cc8
Salman Asif <salmanasif36@gmail.com>
2021-03-08 17:23:37 +0500

    initialize the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('Salman');
   return (
     <div className="App">
       <header className="App-header">
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
+import React from 'react';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// eslint-disable-next-line no-undef
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
