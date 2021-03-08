e19dce84cd448ee45667992ced26d045b2f59e52
Salman Asif <salmanasif36@gmail.com>
2021-03-08 18:30:50 +0500

    check-1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const obj = 'salman Asif';
-  // console.log('Salman');
   return (
     <div className="App">
       <header className="App-header">
