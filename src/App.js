4b8eaf97c739d4807f2636962a0962d1ac6a117f
Salman Asif <salmanasif36@gmail.com>
2021-03-08 17:29:22 +0500

    error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const obj = 'asd';
+  const obj = 'salman';
   // console.log('Salman');
   return (
     <div className="App">
